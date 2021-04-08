30fdf85385bb5367cb2a53522c9221f3bf01dc2b
jmdmacapagal <jmdmacapagal@gmail.com>
2021-04-08 16:28:00 +0800

    added test var

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
 export const increment = () => {
-    return {
-        type: 'INCREMENT_NUM'
-    }
-}
+  return {
+    type: 'INCREMENT_NUM',
+  };
+};
 
 export const decrement = () => {
-    return {
-        type: 'DECREMENT_NUM'
-    }
-}
+  return {
+    type: 'DECREMENT_NUM',
+    test: 'test',
+  };
+};
